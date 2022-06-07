9acefce1285e8bc5563c9f079e041601de33ca44
Henrique <55263158+hnralmeida@users.noreply.github.com>
2022-06-07 17:59:30 -0300

    Serial communication ino-rpi
    See the txrx.py for the arduino implementation

--- txrx.py
+++ txrx.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Raspberry Pi to Arduino I2C Communication
+# Raspberry Pi to Arduino Serial Communication
 
 import serial
 
 if __name__ == '__main__':
-  ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
   ser.flush()
   file1 = open("data.txt","a")
   
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     if ser.in_waiting > 0:
       line = ser.readline().decode('utf-8').rstrip()
       print(line)
-      filse' = open("data.txt","a")
+      file1 = open("data.txt","a")
       line2 = str(line) + " \n"
       file1.write(line2)
       file1.close()
