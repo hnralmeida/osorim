bce23d0c4cea61e5e428eb66de7f61417803bdd7
Henrique <55263158+hnralmeida@users.noreply.github.com>
2022-08-10 20:40:01 -0300

    update gyro 50%, echo, motor, servo, serial

--- project01.py
+++ project01.py
@@ -48,6 +48,14 @@ m2.start(25)
 
 time.sleep(1)
 
+# Set pin 11 as an output, and set servo1 as pin 11 as PWM
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
+
+#start PWM running, but with value of 0 (pulse off)
+servo1.start(0)
+servo1.ChangeDutyCycle(7)
+
 # funcao para garantir a leitura correta da distancia
 def distFix(dist):
     dist.sort()
@@ -59,9 +67,9 @@ def distControl():
     end_t = 0
     
     g.output(trig,g.LOW)
-    time.sleep(0.2)
+    time.sleep(0.01)
     g.output(trig, g.HIGH)
-    time.sleep(0.2)
+    time.sleep(0.01)
     g.output(trig,g.LOW)
 
     while g.input(echo) == 0 :
@@ -74,6 +82,16 @@ def distControl():
     dist = (tempo * 17150)    ## velocidade = 34300 cm/s
                                 ## ida e volta
     return dist
+
+# funcao que normaliza leituras de distancia do sensor ultrassonico
+def retDist():
+    vDist = [ ]
+    for i in range(1,6) :
+        vDist.append( distControl() )
+        
+    d = distFix(vDist)
+
+    return d
 
 # recebe um parametro que contra o motor
 # aceita os comandos:
@@ -182,7 +200,34 @@ def motor2(x):
     else:
         print("<<<  wrong entry  >>>")
         print("please enter ta valid command to continue.....")
-
+        
+# controle do servo
+# f deixa a 90 graus
+# l deixa a 180 graus
+# r deixa a 0 graus
+def servoD(x):
+    if x == 'r':
+        servo1.ChangeDutyCycle(2)
+    elif x == 'l':
+        servo1.ChangeDutyCycle(13)
+    elif x == 'f':
+        servo1.ChangeDutyCycle(7)
+        
+# checa uma rotacao de 90 graus
+def rotate90(ser) :
+    rot = 0
+    while rot < 1.57 and rot > (-1.57):
+        try:
+            line = float(ser.readline().decode('utf-8').rstrip())
+            print("\n")
+            print(line, rot)
+            line = float(line)*0.1 
+            rot = line + rot
+        except (KeyboardInterrupt):
+            break
+        except:
+            pass
+    
 # funcao principal de teste 
 def main():
     # for serial reading is used
@@ -191,12 +236,36 @@ def main():
     ser.flush()
     
     while(1) :
-        dist = [ ]
-        for i in range(1,6) :
-            dist.append( distControl() )
             
-        distFix(dist)
+        d = retDist()
         
+        if d<16 :
+            servoD('r')
+            sleep(0.5)
+            d = retDist()
+            if d<16 :
+                servoD('l')
+                sleep(0.5)
+                d = retDist()
+                if d<16 :
+                    print("Dead End")
+                else:
+                    motor1('s')
+                    motor2('f')
+                    rotate90(ser)
+                    motor1('f')
+                    motor2('f')
+            else:
+                motor1('f')
+                motor2('s')
+                rotate90(ser)
+                motor1('f')
+                motor2('f')
+            else:
+                motor1('f')
+                motor2('f')
+        servoD('f')
+        sleep(0.5)
         ###
     
     ser.close()
